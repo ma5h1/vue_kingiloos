1cd162b262a4f07803e71171897afcc1e3e03f22
Artur <arturnoorlind@gmail.com>
2020-11-25 15:44:32 +0200

    stuff2

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -2,31 +2,42 @@
   <div class="event">
     <h1>Tere tulemast kingiloosi veebilehele!</h1>
     <h1>Siin saad luua enda ürituse jaoks kinkide loosi.</h1>
-    <label for="event_date">Ürituse kuupäev</label>
     <p>
+      <select v-model="event.language"  id="event_language">
+        <option disabled value="">Vali ürituse keel</option>
+        <option>Eesti</option>
+        <option>English</option>
+        <option>Русский</option>
+      </select>
+    </p>
+    <p>
+      <label for="event_date">Ürituse kuupäev</label><br>
       <input class ="sisend" id="event_date" v-model="event.date" type="date">
     </p>
     <p>
       <input class ="sisend" v-model="event.location" type="text" placeholder="Sisesta ürituse asukoht">
     </p>
-    <p>
-      <input class ="sisend" v-model="event.nrparticipants" type="number" min="1" placeholder="Sisesta osalejate arv">
-    </p>
-    <p>
-      <input class ="sisend" v-model="event.language" type="string" placeholder="Sisesta ürituse keel">
-    </p>
-    <p1>
-      <input class ="sisend1" v-model="participant.name" type="string" placeholder="Sisesta osaleja nimi">
-    </p1>
-    <p1>
-      <input class ="sisend1" v-model="participant.email" type="email" placeholder="Sisesta osaleja email">
-    </p1>
-    <p1>
-      <input class ="sisend1" v-model="participant.language" type="string" placeholder="Sisesta osaleja keel">
-    </p1>
     <p1>
       <button class ="sisend1" v-on:click="addParticipant()">Lisa osaleja</button>
     </p1>
+    <table align="center">
+      <tr v-for="participant in event.participants">
+        <td>
+          <input class ="sisend1" v-model="participant.name" type="string" placeholder="Sisesta osaleja nimi">
+        </td>
+        <td>
+          <input class ="sisend1" v-model="participant.email" type="email" placeholder="Sisesta osaleja email">
+        </td>
+        <td>
+          <select v-model="participant.language" >
+            <option disabled value="">Vali osaleja keel</option>
+            <option>Eesti</option>
+            <option>English</option>
+            <option>Русский</option>
+          </select>
+        </td>
+      </tr>
+    </table>
     <p>
     <input class ="checkbox" type="checkbox" id="checkbox">
     <label for="checkbox">Jah, mul on luba kasutada kingiloosi jaoks osaleja(te) e-posti aadresse</label>
@@ -40,11 +51,8 @@
 <script>
 
 let addParticipantFunction = function () {
-alert ("Lisa osaleja!")
+  this.event.participants.push({language: ""});
 }
-
-
-
 
 export default {
   methods: {
@@ -52,8 +60,9 @@ export default {
   },
   data: function () {
     return {
-      event: {},
-      participant: {}
+      event: { language:"",
+        participants: []
+      }
     }
   }
 }
