1ebcdc55b5b8f906b03b538323673a692a8d9939
Artur <arturnoorlind@gmail.com>
2020-11-25 14:08:10 +0200

    stuff2

---
+++ src/views/Events.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="event">
+    <h1>Tere tulemast kingiloosi veebilehele!</h1>
+    <h1>Siin saad luua enda ürituse jaoks kinkide loosi.</h1>
+    <p>
+      <input class ="sisend" v-model="event.date" type="date" placeholder="Sisesta ürituse kuupäev">
+    </p>
+    <p>
+      <input class ="sisend" v-model="event.location" type="text" placeholder="Sisesta ürituse asukoht">
+    </p>
+    <p>
+      <input class ="sisend" v-model="event.nrparticipants" type="number" placeholder="Sisesta osalejate arv">
+    </p>
+    <p>
+      <input class ="sisend" v-model="event.language" type="string" placeholder="Sisesta ürituse keel">
+    </p>
+    <p1>
+      <input class ="sisend1" v-model="participant.name" type="string" placeholder="Sisesta osaleja nimi">
+    </p1>
+    <p1>
+      <input class ="sisend1" v-model="participant.email" type="email" placeholder="Sisesta osaleja email">
+    </p1>
+    <p1>
+      <input class ="sisend1" v-model="participant.language" type="string" placeholder="Sisesta osaleja keel">
+    </p1>
+    <p>
+    <input class ="checkbox" type="checkbox" id="checkbox">
+    <label for="checkbox">Jah, mul on luba kasutada kingiloosi jaoks osaleja(te) e-posti aadresse</label>
+    </p>
+    <button v-on:click="createEvent()">Loo üritus!</button>
+    <br><br><br>
+
+  </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+  methods: {
+  },
+  data: function () {
+    return {
+      event: {},
+      participant: {}
+    }
+  }
+}
+</script>
